ec32e0bd9d3b310b9a10e6bc47ef793718633312
Rob <dudleyr4@outlook.com>
2015-07-01 11:53:09 +0100

    All code files included

---
+++ SimpleFTP/Form1.Designer.vb
@@ -0,0 +1,236 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtServer = New System.Windows.Forms.TextBox()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(16, 58)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(598, 316)
+        Me.ListBox1.TabIndex = 1
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripStatusLabel2})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 413)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(633, 22)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(503, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(111, 20)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Connect to server"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(16, 380)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(133, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Download selected file"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(155, 380)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Upload file"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(236, 380)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(112, 23)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "Delete selected file"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(354, 380)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(94, 23)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "Refresh file list"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(0, 17)
+        '
+        'ToolStripStatusLabel2
+        '
+        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(0, 17)
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(66, 32)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(548, 20)
+        Me.TextBox1.TabIndex = 8
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Address:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Server:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(165, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Username:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(335, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(56, 13)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Password:"
+        '
+        'txtServer
+        '
+        Me.txtServer.Location = New System.Drawing.Point(59, 6)
+        Me.txtServer.Name = "txtServer"
+        Me.txtServer.Size = New System.Drawing.Size(100, 20)
+        Me.txtServer.TabIndex = 13
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(229, 6)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(100, 20)
+        Me.txtUsername.TabIndex = 14
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(397, 6)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(100, 20)
+        Me.txtPassword.TabIndex = 15
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(633, 435)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.txtServer)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel2 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtServer As System.Windows.Forms.TextBox
+    Friend WithEvents txtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+
+End Class
---
+++ SimpleFTP/Form1.vb
@@ -0,0 +1,132 @@
+﻿Imports System.Net
+Imports System.IO
+
+Public Class Form1
+    Private Sub listFTP(URL As String, username As String, password As String)
+        Dim request As FtpWebRequest = Nothing
+        Dim response As FtpWebResponse = Nothing
+        Dim reader As StreamReader = Nothing
+        Try
+            request = CType(WebRequest.Create(URL), WebRequest)
+            request.Credentials = New NetworkCredential(username, password)
+            request.Method = WebRequestMethods.Ftp.ListDirectory
+            response = CType(request.GetResponse(), FtpWebResponse)
+            reader = New StreamReader(response.GetResponseStream)
+            While reader.Peek > -1
+                ListBox1.Items.Add(reader.ReadLine())
+            End While
+            ToolStripStatusLabel1.Text = "Listing complete!"
+        Catch ex As UriFormatException
+            ToolStripStatusLabel1.Text = ex.Message
+        Catch ex As WebException
+            ToolStripStatusLabel2.Text = ex.Message
+        Finally
+            If reader IsNot Nothing Then reader.Close()
+        End Try
+    End Sub
+
+    Private Sub downloadFTP(URL As String, username As String, password As String)
+        Dim request As FtpWebRequest = Nothing
+        Dim response As FtpWebResponse = Nothing
+        Dim responseStream As Stream = Nothing
+        Dim fileStream As FileStream = Nothing
+        Try
+            request = CType(WebRequest.Create(URL), FtpWebRequest)
+            request.Credentials = New NetworkCredential(username, password)
+            request.Method = WebRequestMethods.Ftp.DownloadFile
+            response = CType(request.GetResponse(), FtpWebResponse)
+            responseStream = response.GetResponseStream
+            SaveFileDialog1.FileName = Path.GetFileName(request.RequestUri.LocalPath)
+            If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+                fileStream = File.Create(SaveFileDialog1.FileName)
+                Dim buff(1024) As Byte
+                Dim bytesRead As Integer = 0
+                While True
+                    bytesRead = responseStream.Read(buff, 0, buff.Length)
+                    If bytesRead = 0 Then Exit While
+                    fileStream.Write(buff, 0, bytesRead)
+                End While
+                ToolStripStatusLabel1.Text = "Download complete!"
+            End If
+        Catch ex As UriFormatException
+            ToolStripStatusLabel1.Text = ex.Message
+        Catch ex As WebException
+            ToolStripStatusLabel2.Text = ex.Message
+        Catch ex As IOException
+            ToolStripStatusLabel2.Text = ex.Message
+        Finally
+            If responseStream IsNot Nothing Then responseStream.Close()
+            If fileStream IsNot Nothing Then fileStream.Close()
+        End Try
+    End Sub
+
+    Private Sub uploadFTP(URL As String, filename As String, username As String, password As String)
+        Dim request As System.Net.FtpWebRequest = DirectCast(WebRequest.Create(URL), FtpWebRequest)
+        request.Credentials = New NetworkCredential(username, password)
+        request.Method = WebRequestMethods.Ftp.UploadFile
+
+        Dim file() As Byte = System.IO.File.ReadAllBytes(filename)
+
+        Dim strz As System.IO.Stream = request.GetRequestStream()
+        strz.Write(file, 0, file.Length)
+        strz.Close()
+        strz.Dispose()
+    End Sub
+
+    Private Sub deleteFTP(URL As String, username As String, password As String)
+        Dim request As FtpWebRequest = Nothing
+        Dim response As FtpWebResponse = Nothing
+        Try
+            request = CType(WebRequest.Create(URL), FtpWebRequest)
+            request.Credentials = New NetworkCredential(username, password)
+            request.Method = WebRequestMethods.Ftp.DeleteFile
+            response = CType(request.GetResponse(), FtpWebResponse)
+            ToolStripStatusLabel1.Text = "Delete complete!"
+        Catch ex As UriFormatException
+            ToolStripStatusLabel1.Text = ex.Message
+        Catch ex As WebException
+            ToolStripStatusLabel2.Text = ex.Message
+        Finally
+            If response IsNot Nothing Then response.Close()
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Clear()
+        listFTP(txtServer.Text, txtUsername.Text, txtPassword.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        downloadFTP(TextBox1.Text, txtUsername.Text, txtPassword.Text)
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        TextBox1.Text = txtServer.Text & "/" & ListBox1.SelectedItems(0).ToString()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Dim request As System.Net.FtpWebRequest = DirectCast(System.Net.WebRequest.Create(txtServer.Text), System.Net.FtpWebRequest)
+            request.Credentials = New System.Net.NetworkCredential("user", "password")
+            request.Method = System.Net.WebRequestMethods.Ftp.UploadFile
+
+            Dim file() As Byte = System.IO.File.ReadAllBytes("c:\file.txt")
+
+            Dim strz As System.IO.Stream = request.GetRequestStream()
+            strz.Write(file, 0, file.Length)
+            strz.Close()
+            strz.Dispose()
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If MessageBox.Show("Are you sure you want to delete the selected file?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) = Windows.Forms.DialogResult.Yes Then
+            deleteFTP(TextBox1.Text, txtUsername.Text, txtPassword.Text)
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ListBox1.Items.Clear()
+        listFTP(txtServer.Text, txtUsername.Text, txtPassword.Text)
+    End Sub
+End Class
